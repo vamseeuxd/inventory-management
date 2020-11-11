a81b1284ede491b924ffbbf3d7289443aaa44fdb
DESKTOP-15JEVLE\Vamsi kalyan <vamsi.flex@gmail.com>
2020-11-11 08:15:57 +0530

    First Check In

---
+++ src/app/app.component.ts
@@ -0,0 +1,18 @@
+import {Component, HostListener, ViewChild} from '@angular/core';
+import {AngularFirestore, AngularFirestoreCollection} from '@angular/fire/firestore';
+import {Observable} from 'rxjs';
+import {NgForm} from '@angular/forms';
+import {AngularFirestoreDocument} from '@angular/fire/firestore/document/document';
+import {MatMenuTrigger} from '@angular/material/menu';
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+
+
+
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,72 @@
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {AngularFireModule} from '@angular/fire';
+import {environment} from '../environments/environment';
+import {FormsModule} from '@angular/forms';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+
+import {AlertModule} from 'ngx-bootstrap/alert';
+import {AccordionModule} from 'ngx-bootstrap/accordion';
+import {ButtonsModule} from 'ngx-bootstrap/buttons';
+import {CarouselModule} from 'ngx-bootstrap/carousel';
+import {CollapseModule} from 'ngx-bootstrap/collapse';
+import {DatepickerModule} from 'ngx-bootstrap/datepicker';
+import {BsDropdownModule} from 'ngx-bootstrap/dropdown';
+import {ModalModule} from 'ngx-bootstrap/modal';
+import {PaginationModule} from 'ngx-bootstrap/pagination';
+import {PopoverModule} from 'ngx-bootstrap/popover';
+import {ProgressbarModule} from 'ngx-bootstrap/progressbar';
+import {RatingModule} from 'ngx-bootstrap/rating';
+import {SortableModule} from 'ngx-bootstrap/sortable';
+import {TabsModule} from 'ngx-bootstrap/tabs';
+import {TimepickerModule} from 'ngx-bootstrap/timepicker';
+import {TooltipModule} from 'ngx-bootstrap/tooltip';
+import {TypeaheadModule} from 'ngx-bootstrap/typeahead';
+import {DragDropModule} from '@angular/cdk/drag-drop';
+
+@NgModule({
+  declarations: [
+    AppComponent
+
+  ],
+  entryComponents: [],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    FormsModule,
+    DragDropModule,
+    MatMenuModule,
+    MatIconModule,
+    MatButtonModule,
+    AccordionModule.forRoot(),
+    ButtonsModule.forRoot(),
+    CarouselModule.forRoot(),
+    CollapseModule.forRoot(),
+    DatepickerModule.forRoot(),
+    BsDropdownModule.forRoot(),
+    ModalModule.forRoot(),
+    PaginationModule.forRoot(),
+    PopoverModule.forRoot(),
+    ProgressbarModule.forRoot(),
+    RatingModule.forRoot(),
+    SortableModule.forRoot(),
+    TabsModule.forRoot(),
+    TimepickerModule.forRoot(),
+    TooltipModule.forRoot(),
+    TypeaheadModule.forRoot(),
+    AlertModule.forRoot(),
+
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {
+}
