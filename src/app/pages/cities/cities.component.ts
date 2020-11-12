caf01740f0c242fac0d65f6dd416fb388465243a
DESKTOP-15JEVLE\Vamsi kalyan <vamsi.flex@gmail.com>
2020-11-12 13:13:53 +0530

    Donw with Cities Module

--- src/app/pages/cities/cities.component.ts
+++ src/app/pages/cities/cities.component.ts
@@ -1,4 +1,15 @@
 import { Component, OnInit } from '@angular/core'
+import {
+  CountriesService,
+  ICountry,
+} from '../countries/service/countries.service'
+import { MatSnackBar } from '@angular/material/snack-bar'
+import { NgForm } from '@angular/forms'
+import { ModalDirective } from 'ngx-bootstrap/modal'
+import { CitiesService, ICity } from './service/cities.service'
+import { StateService } from '../state/service/state.service'
+import { BehaviorSubject, combineLatest, of } from 'rxjs'
+import { switchMap } from 'rxjs/operators'
 
 @Component({
   selector: 'app-cities',
@@ -6,7 +17,78 @@ import { Component, OnInit } from '@angular/core'
   styleUrls: ['./cities.component.scss'],
 })
 export class CitiesComponent implements OnInit {
-  constructor() {}
+  cityToEdit: ICity = { name: '', deleted: false, id: '' }
+  selectedCountry$: BehaviorSubject<ICountry> = new BehaviorSubject<ICountry>(
+    null
+  )
+  statesList$ = combineLatest(
+    this.stateService.getStates(),
+    this.selectedCountry$
+  ).pipe(
+    switchMap(([states, country]) =>
+      of(
+        states.filter((state) => {
+          // @ts-ignore
+          if (state.country && state.country.id && country) {
+            // @ts-ignore
+            return state.country.id === country
+          } else {
+            return false
+          }
+        })
+      )
+    )
+  )
+
+  constructor(
+    public cityService: CitiesService,
+    public countriesService: CountriesService,
+    public stateService: StateService,
+    private _snackBar: MatSnackBar
+  ) {}
 
   ngOnInit(): void {}
+
+  async addCity(value: ICity, form: NgForm, modal: ModalDirective = null) {
+    try {
+      await this.cityService.addCities(value)
+      form.resetForm({})
+      if (modal) {
+        modal.hide()
+      }
+      this._snackBar.open('City Added Successfully', 'Success', {
+        duration: 2000,
+      })
+    } catch (e) {
+      alert(e)
+    }
+  }
+
+  async deleteCity(value: ICity) {
+    const isConfirmed = confirm('Are you sure! Do you want to delete the City')
+    if (isConfirmed) {
+      try {
+        await this.cityService.deleteCities(value)
+        this._snackBar.open('City Deleted Successfully', 'Success', {
+          duration: 2000,
+        })
+      } catch (e) {
+        alert(e)
+      }
+    }
+  }
+
+  async updateCity(value: ICity, form: NgForm, modal: ModalDirective) {
+    try {
+      await this.cityService.updateCities(value)
+      form.resetForm({})
+      modal.hide()
+      this.cityToEdit = { name: '', deleted: false, id: '' }
+      this._snackBar.open('City Updated Successfully', 'Success', {
+        duration: 2000,
+      })
+    } catch (e) {
+      alert(e)
+    }
+  }
 }
--- src/app/pages/cities/service/cities.service.ts
+++ src/app/pages/cities/service/cities.service.ts
@@ -1,8 +1,148 @@
 import { Injectable } from '@angular/core'
+import {
+  AngularFirestore,
+  AngularFirestoreCollection,
+} from '@angular/fire/firestore'
+import { Observable } from 'rxjs'
+import { UsersService } from '../../users/service/users.service'
+import {
+  CountriesService,
+  ICountry,
+} from '../../countries/service/countries.service'
+import { BusyIndicatorService } from '../../../shared/busy-indicator/busy-indicator.service'
+import { leftJoinDocument } from '../../../shared/utilities/collectionJoin'
+import * as firebase from 'firebase'
+import { IState } from '../../state/service/state.service'
+
+export interface ICity {
+  name: string
+  id?: string
+  country?: string | ICountry
+  state?: string | IState
+  deleted: boolean
+  createdOn?: string
+  updatedOn?: string
+  createdBy?: string
+  updatedBy?: string
+}
 
 @Injectable({
   providedIn: 'root',
 })
 export class CitiesService {
-  constructor() {}
+  private citiesAction: AngularFirestoreCollection<ICity>
+  private readonly cities123$: Observable<ICity[]>
+  private activeUser: string
+  private readonly cities$: Observable<ICity[]>
+
+  constructor(
+    firestore: AngularFirestore,
+    private usersService: UsersService,
+    private countriesService: CountriesService,
+    public busyIndicator: BusyIndicatorService
+  ) {
+    this.citiesAction = firestore.collection<ICity>('cities')
+    // @ts-ignore
+    this.cities$ = firestore
+      .collection<ICity>('cities', (ref) => {
+        return ref.where('deleted', '==', false).orderBy('createdOn')
+      })
+      .valueChanges()
+      .pipe(
+        leftJoinDocument(firestore, 'country', 'countries'),
+        leftJoinDocument(firestore, 'state', 'states')
+      )
+    this.usersService.activeUser$.subscribe((activeUser) => {
+      this.activeUser = activeUser
+    })
+  }
+
+  addCities(city: ICity): Promise<any> {
+    return new Promise(async (resolve, reject) => {
+      if (city.name.trim().length > 0) {
+        const busyIndicatorId = this.busyIndicator.show()
+        const docRef = this.citiesAction.ref.doc()
+        const name = city.name.trim()
+        const id = docRef.id
+        const country = city.country
+        const state = city.state
+        const deleted = false
+        const createdOn = this.getServerTime()
+        const updatedOn = this.getServerTime()
+        const createdBy = this.activeUser
+        const updatedBy = this.activeUser
+        try {
+          await docRef.set({
+            name,
+            id,
+            country,
+            state,
+            createdOn,
+            updatedOn,
+            createdBy,
+            updatedBy,
+            deleted,
+          })
+          this.busyIndicator.hide(busyIndicatorId)
+          resolve(id)
+        } catch (e) {
+          this.busyIndicator.hide(busyIndicatorId)
+          reject(e)
+        }
+      }
+    })
+  }
+
+  updateCities(city: ICity): Promise<any> {
+    return new Promise(async (resolve, reject) => {
+      if (city.name.trim().length > 0) {
+        const busyIndicatorId = this.busyIndicator.show()
+        const docRef = this.citiesAction.doc(city.id).ref
+        const name = city.name.trim()
+        const country = city.country
+        const state = city.state
+        const updatedOn = this.getServerTime()
+        const updatedBy = this.activeUser
+        try {
+          await docRef.update({ name, country, state, updatedOn, updatedBy })
+          resolve(docRef.id)
+          this.busyIndicator.hide(busyIndicatorId)
+        } catch (e) {
+          this.busyIndicator.hide(busyIndicatorId)
+          reject(e)
+        }
+      }
+    })
+  }
+
+  deleteCities(city: ICity): Promise<any> {
+    return new Promise(async (resolve, reject) => {
+      const busyIndicatorId = this.busyIndicator.show()
+      try {
+        const docRef = await this.citiesAction.doc(city.id)
+        const doc = await docRef.get().toPromise()
+        const updatedOn = this.getServerTime()
+        const updatedBy = this.activeUser
+        await docRef.set({
+          ...doc.data(),
+          deleted: true,
+          updatedOn,
+          updatedBy,
+        })
+        resolve(doc.id)
+        this.busyIndicator.hide(busyIndicatorId)
+      } catch (e) {
+        this.busyIndicator.hide(busyIndicatorId)
+        reject(e)
+      }
+    })
+  }
+
+  getCities(): Observable<ICity[]> {
+    return this.cities$
+  }
+
+  getServerTime(): any {
+    return firebase.firestore.Timestamp.now().seconds * 1000
+  }
 }
